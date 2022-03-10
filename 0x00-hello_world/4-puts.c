5b7ee1585c5f858dbe1f2d4cb095efd33c527679
Ayomione <oguntade2001@gmail.com>
2022-03-11 00:35:54 +0100

    jesus

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  * main - Entry point
  * Description: prints "Programming is like building a multilingual puzzle"
  * Return: Always 0 (Success)
- */
-	int main(void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
+ **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"):
 		return (0);
-} 
+}
