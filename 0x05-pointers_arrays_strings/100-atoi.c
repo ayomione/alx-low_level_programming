91962f998a18f69d49f7098ad6d693c64bf40f17
Ayomione <oguntade2001@gmail.com>
2022-03-22 02:12:48 +0100

    principles commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -27,12 +27,12 @@ int _atoi(char *s)
 		count++;
 	}
 
-	for (i = count - size; i < couunt; i++)
+	for (i = count - size; i < count; i++)
 	{
 		oi = oi + ((*(s + i) - 48) * m);
 		m /= 10;
 	}
 	return (oi * pn);
-}`
+}
 
 		
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,10 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] i= '\0')
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
-			_putchar(str[str[i]);
+			_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');
